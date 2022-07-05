fc0c9a623d48a1d9837a41116243f81790787364
Alondra87 <aa5887687@gmail.com>
2022-07-05 11:57:31 -0400

    Features

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ const LoadToDoList = () => {
   showList.appendChild(containerToDoList);
 };
 
+LoadToDoList();
+
 const AddTaskToList = (task, index, done) => {
   const containerTasks = document.createElement('div');
   containerTasks.innerHTML += `
@@ -22,9 +24,6 @@ const AddTaskToList = (task, index, done) => {
       `;
   showList.appendChild(containerTasks);
 };
-
-LoadToDoList();
-AddTaskToList();
 
 let arrayTask = [];
 
@@ -62,9 +61,25 @@ CountCheckbox.addEventListener('keypress', (event) => {
   }
 });
 
+
+
 document.addEventListener('change', (event) => {
   CheckBoxesChecked(event);
 });
+
+
+const CheckBoxesChecked = (event) => {
+  if (event.target.classList.contains('checkbox')) {
+    const list = JSON.parse(localStorage.getItem('mytodoList'));
+
+    list[event.target.id - 1].done = !list[event.target.id - 1].done;
+    event.target.parentNode.classList.toggle('newbackg');
+    event.target.nextElementSibling.classList.toggle('input-done');
+    event.target.nextElementSibling.classList.toggle('done-task');
+    event.target.parentNode.lastElementChild.classList.toggle('trash');
+    localStorage.setItem('mytodoList', JSON.stringify(list));
+  }
+};
 
 document.addEventListener('click', (event) => {
   if (event.target.classList.contains('fa-trash-alt')) {
